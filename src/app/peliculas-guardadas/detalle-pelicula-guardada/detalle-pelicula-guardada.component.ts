f8d60bf64cd57c48183ceb7ee2efeda98199b8a0
Breinner09 <breinnerpg09@gmail.com>
2023-12-29 15:13:22 -0500

    Front

--- src/app/peliculas-guardadas/detalle-pelicula-guardada/detalle-pelicula-guardada.component.ts
+++ src/app/peliculas-guardadas/detalle-pelicula-guardada/detalle-pelicula-guardada.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Pelicula } from 'src/app/Interfaces/pelicula';
 import { PeliculasService } from 'src/app/services/peliculas.service';
 import Swal from 'sweetalert2';
 
